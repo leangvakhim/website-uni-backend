0caf273192b4b8fe1d6fceebe4fd9a97d10bb6f9
Sokunnidsa-Pheng <phengsokunnidsaa@gmail.com>
2025-04-07 09:37:47 +0700

    social

--- app/Models/Social.php
+++ app/Models/Social.php
@@ -30,7 +30,6 @@ class Social extends Model
         return $this->belongsTo(Faculty::class, 'social_faculty', 'f_id');
     }
 
-    }   
     public function setting()
     {
         return $this->hasMany(Setting::class, 'set_social', 'social_id');
