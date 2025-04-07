162391fbf9393ea205e9e9967041ba406b182759
Khim070 <Vakhiml60@gmail.com>
2025-04-07 23:08:40 +0700

    Page

--- app/Http/Requests/PageRequest.php
+++ app/Http/Requests/PageRequest.php
@@ -19,23 +19,13 @@ class PageRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
+
     public function rules(): array
     {
         return [
-            'p_menu' => 'nullable|integer|exists:tbmenu,menu_id',
-            'p_title' => 'nullable|string|max:50',
+            'p_title' => 'nullable|string|max:255',
             'p_alias' => 'nullable|string',
-            'p_busy' => 'required|boolean',
-            'display' => 'required|boolean',
-            'active' => 'required|boolean',
-
-            'p_menu' => 'nullable|array',
-            'p_menu.title' => 'nullable|string|max:255',
-            'p_menu.menu_order' => 'nullable|integer',
-            'p_menu.menup_id' => 'nullable|integer|exists:tbmenu,menu_id',
-            'p_menu.lang' => 'nullable|integer|in:1,2',
-            'p_menu.display' => 'nullable|boolean',
-            'p_menu.active' => 'nullable|boolean',
+            'p_busy' => 'nullable|boolean',
         ];
     }
 }
