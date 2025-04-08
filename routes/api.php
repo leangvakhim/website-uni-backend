dff8225140d9ef89ef307cb0d7089012e4f5d2e6
Khim070 <Vakhiml60@gmail.com>
2025-04-08 11:46:18 +0700

    api.php

--- routes/api.php
+++ routes/api.php
@@ -80,6 +80,8 @@ Route::prefix('faculty-contact')->group(function () {
     Route::post('/create', [FacultyContactController::class, 'create']);
     Route::post('/update/{id}', [FacultyContactController::class, 'update']);
     Route::put('/visibility/{id}', [FacultyContactController::class, 'visibility']);
+    Route::get('/by-faculty/{f_id}', [FacultyContactController::class, 'getByFaculty']);
+    Route::post('/reorder', [FacultyContactController::class, 'reorder']);
 });
 
 Route::prefix('faculty-bg')->group(function () {
