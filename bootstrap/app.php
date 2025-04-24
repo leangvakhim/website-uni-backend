3fe78c46736e12c5e0581654d6b571cec7a6e7fc
Khim070 <Vakhiml60@gmail.com>
2025-04-24 14:05:19 +0700

    apply cors-origin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
     })
     // ->withMiddleware(function (Middleware $middleware) {
     //     $middleware->prepend(\Illuminate\Http\Middleware\HandleCors::class);
