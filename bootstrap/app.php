957bd33d25f1dd384be73cb534e0dff650d9b429
Khim070 <Vakhiml60@gmail.com>
2025-04-24 14:44:37 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,3 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+$app->loadConfigurationFiles(['cors']);
+
+return $app;
